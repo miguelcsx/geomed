dd58b3d8648bc656e8e29cc9f6b0221000c3b141
GabrielAGES <cosasudemga@gmail.com>
2023-10-31 17:44:37 -0500

    Update package description

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
   {
     path: "/basics/postulates/incidence",
     element: <LinkingPostulates/>
-  }
+  },
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
